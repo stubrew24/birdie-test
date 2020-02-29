6baf8100437619f2240f679beaa557cdfafc304f
Stuart Sewell <hello@stuartsewell.dev>
2020-02-29 12:54:27 +0000

    backend cors

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,8 +1,11 @@
+import * as cors from 'cors';
 import * as express from "express";
 import { eventsController } from "./controllers/events";
 import { pingController } from "./controllers/ping";
 
 const app = express();
+
+app.use(cors())
 
 app.use(eventsController)
 app.use(pingController);
